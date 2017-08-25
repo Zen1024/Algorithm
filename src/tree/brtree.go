5a7aa36675af14ad8f9a7a7f632353a4e8df910a
Zen1024 <1067109824@qq.com>
2017-08-25 17:15:32 +0800

    br-tree insert

--- src/tree/brtree.go
+++ src/tree/brtree.go
@@ -33,6 +33,8 @@ func (t *BrNode) LeftRotate() {
 		} else {
 			x.Parent.Right = y
 		}
+	} else {
+
 	}
 
 	//x下移
@@ -130,62 +132,288 @@ func (t *BrNode) get(elem int) *BrNode {
 	}
 	return re
 }
-
-func (t *BrNode) insert_fix(elem *BrNode) {
-	p := elem
-	if t == elem {
-		return
+func (t *BrNode) plrec() (p *BrNode, l *BrNode, r *BrNode, e int, c Color) {
+	if t == nil {
+		return nil, nil, nil, -1, ColorBlack
 	}
-	if p.Parent.Parent == nil {
-		return
-	}
+	return t.Parent, t.Left, t.Right, t.Elem, t.Color
 
-	for p.Parent.Color == ColorRed {
-		if p.Parent.Parent == nil {
-			return
-		}
-		var uncle *BrNode
-		if p.Parent == p.Parent.Parent.Left {
-			uncle = p.Parent.Parent.Right
-		} else {
-			uncle = p.Parent.Parent.Left
-		}
-		if uncle == nil {
-			return
-		}
-		if uncle.Color == ColorRed {
-			uncle.Color = ColorBlack
-			p.Parent.Color = ColorBlack
-			p.Parent.Parent.Color = ColorRed
-		}
-
-		if p.Parent == p.Parent.Parent.Left {
-			if uncle.Color == ColorBlack {
-				if p.Parent.Right == p {
-					p.Parent.LeftRotate()
-				}
-				p.Parent.Color = ColorBlack
-				p.Parent.Parent.Color = ColorRed
-				p.Parent.Parent.RightRotate()
-				p = p.Parent
-			}
-		} else {
-			if uncle.Color == ColorBlack {
-				if p.Parent.Right == p {
-					p.Parent.LeftRotate()
-				}
-				p.Parent.Color = ColorBlack
-				if p.Parent.Parent == nil {
-					fmt.Printf("p:%4d", p.Elem)
-				}
-				p.Parent.Parent.Color = ColorRed
-				p.Parent.Parent.LeftRotate()
-				p = p.Parent
-			}
-		}
-	}
 }
-func (t *BrNode) Insert(elem int) {
+
+func (t *BrNode) elem() int {
+	if t == nil {
+		return -1
+	}
+	return t.Elem
+}
+func (t *BrNode) print(name string) {
+	p, l, r, e, c := t.plrec()
+	fmt.Printf("[%s-%d][%p] p[%d:%p],l:[%d:%p],r:[%d:%p],c[%s]\n", name, e, t, p.elem(), p, l.elem(), l, r.elem(), r, c.color())
+}
+
+func (c Color) color() string {
+	if c == ColorBlack {
+		return "b"
+	}
+	return "r"
+}
+
+func (t *BrNode) Valid() bool {
+	leaves := t.leaves()
+	ll := len(leaves)
+	if ll == 0 {
+		return false
+	}
+	cnt := t.pathBlackCnt(leaves[0])
+
+	for i := 0; i < ll; i++ {
+		c := t.pathBlackCnt(leaves[i])
+		if c != cnt {
+			return false
+		}
+	}
+	return true
+
+}
+
+//get leaves
+func (t *BrNode) leaves() []*BrNode {
+	var p *BrNode = t
+	re := []*BrNode{}
+	for p != nil {
+		if p.Left == nil {
+			if p.Right == nil {
+				re = append(re, p)
+			}
+			p = p.Right
+		} else {
+			pre := p.Left
+			for pre.Right != nil && pre.Right != p {
+				pre = pre.Right
+			}
+			if pre.Right == nil {
+				if pre.Left == nil {
+					re = append(re, pre)
+				}
+				pre.Right = p
+				p = p.Left
+			} else {
+				pre.Right = nil
+				p = p.Right
+			}
+		}
+	}
+	return re
+}
+
+func (t *BrNode) pathBlackCnt(leaf *BrNode) int {
+	if t.Parent != nil || leaf == nil {
+		return -1
+	}
+	var p *BrNode = t
+	re := 0
+
+	for p != nil {
+		if p.Color == ColorBlack {
+			re++
+		}
+		if p.Elem == leaf.Elem {
+			return re
+		}
+		if p.Elem < leaf.Elem {
+			p = p.Right
+		} else {
+			p = p.Left
+		}
+	}
+	return -1
+
+}
+
+func (t *BrNode) min() *BrNode {
+	if t == nil {
+		return nil
+	}
+	if t.Left == nil {
+		return t
+	}
+	p := t
+
+	for p.Left != nil {
+		p = p.Left
+	}
+	return p
+}
+
+func (t *BrNode) max() *BrNode {
+	if t == nil {
+		return nil
+	}
+	if t.Right == nil {
+		return t
+	}
+	p := t
+	for p.Right != nil {
+		p = p.Right
+	}
+	return p
+}
+
+//前驱
+func (t *BrNode) predecessor() *BrNode {
+	if t == nil {
+		return nil
+	}
+	if t.Left != nil {
+		return t.Left.max()
+	}
+	var p, x *BrNode
+	p = t.Parent
+	x = t
+	if p == nil {
+		return nil
+	}
+	for p.Parent != nil && x == p.Left {
+		x = p
+		p = p.Parent
+	}
+	return p
+
+}
+
+//后继
+func (t *BrNode) successor() *BrNode {
+	if t == nil {
+		return nil
+	}
+	if t.Right != nil {
+		return t.Right.min()
+	}
+	var p, x *BrNode
+	p = t.Parent
+	x = t
+	if p == nil {
+		return nil
+	}
+	for p.Parent != nil && x == p.Right {
+		x = p
+		p = p.Parent
+	}
+	return p
+
+}
+
+func (t *BrNode) find(elem int) *BrNode {
+	var p *BrNode = t
+	if p.elem() == elem {
+		return p
+	}
+	for p != nil {
+		if p.Elem == elem {
+			return p
+		}
+		if p.Elem > elem {
+			p = p.Left
+		} else {
+			p = p.Right
+		}
+	}
+	return nil
+}
+func (t *BrNode) insert_fix(elem *BrNode) *BrNode {
+	if elem == nil {
+		return t
+	}
+	if elem.Parent == nil {
+		elem.Color = ColorBlack
+		return t
+	}
+	// fmt.Printf("fixing %d...\n", elem.Elem)
+	z := elem
+	for p := z.Parent; p.Color == ColorRed; {
+		var uncle *BrNode
+		if p == p.Parent.Left {
+			uncle = p.Parent.Right
+		} else {
+			uncle = p.Parent.Left
+		}
+		// p.print("p")
+		// z.print("z")
+		if uncle != nil {
+			//cond -1
+			// uncle.print("uncle")
+			if uncle.Color == ColorRed {
+				p.Parent.Color = ColorRed
+				p.Color = ColorBlack
+				uncle.Color = ColorBlack
+				z = p.Parent.Parent
+				continue
+			}
+		}
+		// fmt.Printf("[init]tree:----------------\n")
+		// t.Print()
+		// fmt.Printf("[init]---------------------\n")
+		//uncle ==nil or uncle-color ==black //cond-2
+		if p == p.Parent.Left {
+			if z == z.Parent.Right {
+				p.LeftRotate()
+				// fmt.Printf("[cond-1]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-1]---------------------\n")
+				z.Parent.RightRotate()
+				// fmt.Printf("[cond-1.1]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-1.1]---------------------\n")
+				z.Color = ColorBlack
+				z.Right.Color = ColorRed
+			} else {
+				p.Parent.RightRotate()
+				// fmt.Printf("[cond-1.2]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-1.2]---------------------\n")
+				p.Color = ColorBlack
+				p.Right.Color = ColorRed
+
+			}
+		} else if p == p.Parent.Right {
+			if z == z.Parent.Left {
+				p.RightRotate()
+				// fmt.Printf("[cond-2]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-2]---------------------\n")
+				// p.print("p")
+				// z.print("z")
+				z.Parent.LeftRotate()
+				// p.print("p")
+				// z.print("z")
+				// fmt.Printf("[cond-2.1]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-2.1]---------------------\n")
+				z.Color = ColorBlack
+				z.Left.Color = ColorRed
+			} else {
+				p.Parent.LeftRotate()
+				// fmt.Printf("[cond-2.2]tree:----------------\n")
+				// t.Print()
+				// fmt.Printf("[cond-2.2]---------------------\n")
+				p.Color = ColorBlack
+				p.Left.Color = ColorRed
+			}
+		}
+	}
+
+	// fmt.Printf("fix %d end\n", elem.Elem)
+	for t.Parent != nil {
+		t = t.Parent
+	}
+	t.Color = ColorBlack
+	return t
+}
+
+func (t *BrNode) Insert(elem int) *BrNode {
+	if t == nil {
+		return nil
+	}
 	p := t
 	new_node := &BrNode{
 		Left:  nil,
@@ -201,7 +429,7 @@ func (t *BrNode) Insert(elem int) {
 		} else if p.Elem > elem {
 			p = p.Left
 		} else {
-			return
+			return t
 		}
 	}
 	new_node.Parent = ele_parent
@@ -210,10 +438,62 @@ func (t *BrNode) Insert(elem int) {
 	} else {
 		ele_parent.Right = new_node
 	}
-	fmt.Printf("before:%v\n", new_node.Parent)
-	t.insert_fix(new_node)
-	return
+	re := t.insert_fix(new_node)
+	//return root
+	return re
 }
 
-func (t *BrNode) delete_fix(elem int) {}
-func (t *BrNode) Delete(elem int)     {}
+func (t *BrNode) delNodeWith01Child(todel *BrNode) *BrNode {
+	if todel == nil {
+		return t
+	}
+	if todel.Parent == nil {
+		return nil
+	}
+	if todel.Left == nil && todel.Right == nil {
+		if todel.Parent.Left == todel {
+			todel.Parent.Left = nil
+		} else {
+			todel.Parent.Right = nil
+		}
+		todel.Parent = nil
+		return t
+	}
+	var new *BrNode
+	if todel.Left != nil {
+		new = todel.Left
+		todel.Left = nil
+	} else {
+		new = todel.Right
+		todel.Right = nil
+	}
+	new.Parent = todel.Parent
+	if todel.Parent.Left == todel {
+		todel.Parent.Left = new
+	} else {
+		todel.Parent.Right = new
+	}
+	todel.Parent = nil
+	return t
+}
+
+func (t *BrNode) delete_fix(elem *BrNode) *BrNode { return t }
+func (t *BrNode) Delete(elem int) *BrNode {
+	todel := t.find(elem)
+	if todel == nil {
+		return t
+	}
+
+	if !(todel.Left != nil && todel.Right != nil) {
+		t.delNodeWith01Child(todel)
+	} else {
+
+		x := t
+		suc := x.successor()
+
+		x.Elem = suc.Elem
+		t.delNodeWith01Child(suc)
+	}
+
+	return t
+}
--- src/tree/brtree_test.go
+++ src/tree/brtree_test.go
@@ -1,64 +1,141 @@
 package tree
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-func TestRotate(t *testing.T) {
-	root := &BrNode{
+func randArr(l, max int) []int {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	re := make([]int, l)
+	for i := 0; i < l; i++ {
+		re[i] = rnd.Intn(max)
+	}
+	return re
+}
+func timetooks(begin int64) string {
+	delta := time.Now().UnixNano() - begin
+	s := delta / int64(1e9)
+	ms := (delta % 1e9) / int64(1e6)
+	us := (delta % 1e6) / int64(1e3)
+	ns := (delta % 1e3) / int64(1)
+	if s > int64(0) {
+		return fmt.Sprintf("%d.%d.%d.%ds", s, ms, us, ns)
+	}
+	if ms > int64(0) {
+		return fmt.Sprintf("%d.%d.%dms", ms, us, ns)
+	}
+	if us > int64(0) {
+		return fmt.Sprintf("%d.%dus", us, ns)
+	}
+	if ns > int64(0) {
+		return fmt.Sprintf("%dns", ns)
+	}
+	return ""
+}
+
+// func TestRotate(t *testing.T) {
+// 	root := &BrNode{
+// 		Parent: nil,
+// 		Elem:   6,
+// 		Color:  ColorBlack,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	x := &BrNode{
+// 		Parent: root,
+// 		Elem:   2,
+// 		Color:  ColorRed,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	root.Left = x
+// 	lx := &BrNode{
+// 		Parent: x,
+// 		Elem:   1,
+// 		Color:  ColorBlack,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	x.Left = lx
+// 	y := &BrNode{
+// 		Parent: x,
+// 		Elem:   4,
+// 		Color:  ColorBlack,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	x.Right = y
+// 	ly := &BrNode{
+// 		Parent: y,
+// 		Elem:   3,
+// 		Color:  ColorRed,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	y.Left = ly
+// 	ry := &BrNode{
+// 		Parent: y,
+// 		Elem:   5,
+// 		Color:  ColorRed,
+// 		Left:   nil,
+// 		Right:  nil,
+// 	}
+// 	y.Right = ry
+// 	root.Print()
+// 	root.Left.LeftRotate()
+// 	root.Print()
+// 	root.Left.RightRotate()
+// 	root.Print()
+// }
+
+// func TestInsert(t *testing.T) {
+// 	suit := []int{10, 100, 1000, 10000, 100000, 1000000, 10000000}
+// 	max := 10000
+// 	ls := len(suit)
+// 	for i := 0; i < ls; i++ {
+// 		arr := randArr(suit[i], max)
+// 		var tree *BrNode = &BrNode{
+// 			Parent: nil,
+// 			Left:   nil,
+// 			Right:  nil,
+// 			Elem:   arr[0],
+// 			Color:  ColorBlack,
+// 		}
+// 		// t.Logf("%v\n", arr)
+// 		la := len(arr)
+// 		now := time.Now().UnixNano()
+// 		for i := 1; i < la; i++ {
+// 			tree = tree.Insert(arr[i])
+// 		}
+// 		tooks := timetooks(now)
+// 		t.Logf("[%d] elements,took:%s", suit[i], tooks)
+
+// 		if !tree.Valid() {
+// 			t.Fatalf("invalid br-tree!")
+// 			tree.Print()
+// 		}
+// 	}
+
+// }
+
+func TestDelete(t *testing.T) {
+	arr := randArr(10, 100)
+	la := len(arr)
+	tree := &BrNode{
+		Elem:   arr[0],
+		Color:  ColorBlack,
+		Left:   nil,
+		Right:  nil,
 		Parent: nil,
-		Elem:   6,
-		Color:  ColorBlack,
-		Left:   nil,
-		Right:  nil,
 	}
-	x := &BrNode{
-		Parent: root,
-		Elem:   2,
-		Color:  ColorRed,
-		Left:   nil,
-		Right:  nil,
+	for i := 1; i < la; i++ {
+		tree = tree.Insert(arr[i])
 	}
-	root.Left = x
-	lx := &BrNode{
-		Parent: x,
-		Elem:   1,
-		Color:  ColorBlack,
-		Left:   nil,
-		Right:  nil,
-	}
-	x.Left = lx
-	y := &BrNode{
-		Parent: x,
-		Elem:   4,
-		Color:  ColorBlack,
-		Left:   nil,
-		Right:  nil,
-	}
-	x.Right = y
-	ly := &BrNode{
-		Parent: y,
-		Elem:   3,
-		Color:  ColorRed,
-		Left:   nil,
-		Right:  nil,
-	}
-	y.Left = ly
-	ry := &BrNode{
-		Parent: y,
-		Elem:   5,
-		Color:  ColorRed,
-		Left:   nil,
-		Right:  nil,
-	}
-	y.Right = ry
-	root.Print()
-	root.Left.LeftRotate()
-	root.Print()
-	root.Left.RightRotate()
-	root.Print()
-	root.Insert(10)
-	root.Print()
-	root.Insert(40)
-	root.Print()
+	t.Logf("%v", arr)
+	tree.Print()
+	tree.Delete(arr[0])
+	tree.Print()
 }
